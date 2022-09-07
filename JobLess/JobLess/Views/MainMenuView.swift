66daa52875fc308cdb61a0523750f937b777a38e
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2022-09-07 22:21:25 +0200

    Added Main Menu

---
+++ JobLess/JobLess/Views/MainMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuView.swift
+//  JobLess
+//
+//  Created by Cédric Bahirwe on 07/09/2022.
+//
+
+import SwiftUI
+
+struct MainMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuView()
+    }
+}
