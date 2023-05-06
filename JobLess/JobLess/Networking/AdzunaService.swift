b24e949efa1cdfa323edd29d105282b1b070b046
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2023-05-06 21:28:09 +0200

    Minor Fix

---
+++ JobLess/JobLess/Networking/AdzunaService.swift
@@ -0,0 +1,16 @@
+//
+//  AdzunaService.swift
+//  JobLess
+//
+//  Created by Cédric Bahirwe on 06/05/2023.
+//
+
+import Foundation
+
+class SomeGenericClassForFetchingJobs { }
+
+final class AdzunaService: SomeGenericClassForFetchingJobs {
+    private let applicationID = "bc7d92f0"
+    
+    private let applicationKey = "b43e885e2fe081083f6d729aab5b2ed9"
+}
